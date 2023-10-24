a6f0a2fa1a840f0e93cb278c86450be8a6e8dd9c
Ayşenur Elmas <ayse66025@gmail.com>
2023-10-24 19:50:55 +0300

    primeNumber

---
+++ Task/Task1/PrimeNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (IsPrimeNumber(6))
+            {
+                Console.WriteLine("This is a prime number.");
+            }
+            else 
+            {
+                Console.WriteLine("This is not  a prime number.");
+            }
+        }
+
+        private static bool IsPrimeNumber(int number)
+        {
+            bool result = true;
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    result = false;
+                    i = number;
+                }
+            }
+            return result;
+        }
+    }
+}
