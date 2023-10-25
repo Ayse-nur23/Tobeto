a8053af19ae5dd9dfc5db641c2feb859e86ec3e4
Ayşenur Elmas <ayse66025@gmail.com>
2023-10-25 21:24:05 +0300

    recap demo.

--- Task/Task2/RecapDemo1/Form1.cs
+++ Task/Task2/RecapDemo1/Form1.cs
@@ -16,5 +16,38 @@ namespace RecapDemo1
         {
             InitializeComponent();
         }
+
+        //İlk çalışsan blok
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button[,] buttons = new Button[8, 8];
+            int top = 0;
+            int left = 0;
+            for (int i = 0; i < buttons.GetUpperBound(0); i++)
+            {
+                for (global::System.Int32 j = 0; j < buttons.GetUpperBound(0); j++)
+                {
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Width = 50;
+                    buttons[i, j].Height = 50;
+                    buttons[i, j].Left = left;
+                    buttons[i, j].Top = top;
+                    left += 50;
+                    if ((i + j) % 2 == 0)
+                    {
+                        buttons[i, j].BackColor = Color.Black;
+                    }
+                    else
+                    {
+                        buttons[i, j].BackColor = Color.White;
+                    }
+                    this.Controls.Add(buttons[i, j]);
+                }
+                left = 0;
+                top += 50;
+            }
+            
+            
+        }
     }
 }
